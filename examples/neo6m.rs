12bb85f25d18f80e2b593572956ea4b73332a7b9
Harshvardhan1809 <harrygarry2002@gmail.com>
2025-02-03 15:21:27 +0900

    improve the gps

--- examples/neo6m.rs
+++ examples/neo6m.rs
@@ -8,6 +8,7 @@ use esp_idf_svc::hal::uart::*;
 use esp_idf_svc::hal::gpio;
 use esp_idf_svc::hal::task::*;
 use esp_idf_svc::hal::prelude::*;
+use nmea_parser::*;
 
 fn main() {
     esp_idf_svc::sys::link_patches();
@@ -23,22 +24,56 @@ fn main() {
 
     log::info!("Connected to NEO6M");
 
+    let mut buf = [0_u8; 1];
+    let mut rec = Vec::new();
+    let mut parser = NmeaParser::new();
+
     block_on(async {
         loop {        
-            let mut rec = Vec::new();
-
-            // NMEA 0183 message is 82 ASCII characters long, 82 bytes
-            for _i in 0..82 {
-                let mut buf = [0_u8; 1];
-                uart.read(&mut buf).await.unwrap();
+            // All NMEA sentences start with $ mark
+            // https://aprs.gids.nl/nmea/
+            uart.read(&mut buf).await.unwrap();
+            if buf[0] == 36 {
+                if rec.len() !=0 {
+                    // println!("Received NMEA: {:?}", rec);
+                    let mut nmea_sentence = match String::from_utf8(rec.clone()) {
+                        Ok(sentence) => sentence,
+                        Err(_) => {
+                            println!("Invalid sentence");
+                            continue;
+                        }
+                    };
+                    nmea_sentence.pop();
+                    nmea_sentence.pop();
+                    // println!("Received NMEA Sentence {:?}", nmea_sentence);
+                    rec.clear();
+                    
+                    // Parse the sentence
+                    match parser.parse_sentence(&mut nmea_sentence) {
+                        Ok(message) => match message {
+                            ParsedMessage::Gga(gga) => {
+                                println!("Source:    {}",     gga.source);
+                                println!("Latitude:  {:?}°", gga.latitude);
+                                println!("Longitude: {:?}°", gga.longitude);
+                                println!("");
+                            },
+                            ParsedMessage::Rmc(rmc) => {
+                                println!("Source:  {}",        rmc.source);
+                                println!("Speed:   {:?} kts", rmc.sog_knots);
+                                println!("Bearing: {:?}°",       rmc.bearing);
+                                println!("");
+                            },
+                            _ => ()
+                        },
+                        Err(_) => {
+                            println!("Getting Error");
+                        }
+                    };
+                }
+                rec.push(36);
+            } else {
                 rec.extend_from_slice(&buf);
             }
-
-            println!("Received NMEA: {:?}", rec);
-            let nmea_sentence = String::from_utf8(rec).unwrap();
-            println!("Received NMEA Sentence {:?}", nmea_sentence);
-
-            // TODO: Parse the NMEA to get data
         }
     })
 }
