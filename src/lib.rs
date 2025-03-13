c064fa078eefc87b95aa839cae07382a0b2bf8f0
Harshvardhan1809 <harrygarry2002@gmail.com>
2025-03-13 10:30:24 +0900

    everything except GPS and SD Card working

--- src/lib.rs
+++ src/lib.rs
@@ -6,7 +6,7 @@ use esp_idf_svc::hal::i2c::{I2cConfig, I2cDriver, I2c};
 use esp_idf_svc::hal::uart::*;
 use esp_idf_svc::hal::uart::{config as UARTConfig};
 use esp_idf_svc::hal::spi::*;
-use esp_idf_svc::hal::spi::{config::Config as SPIConfig, SpiDriver};
+use esp_idf_svc::hal::spi::{config::Config as SPIConfig, SpiDriver, config::MODE_3, SPI2};
 use esp_idf_svc::hal::task::*;
 use esp_idf_svc::hal::prelude::*;
 use esp_idf_svc::sys::EspError;
@@ -23,8 +23,6 @@ use ina219_rs::physic;
 use nmea_parser::*;
 use embedded_sdmmc::{VolumeManager, FatVolume, SdCard};
 
-use shared_pin::SharedPin;
-
 pub fn run(){
     esp_idf_svc::sys::link_patches();
     esp_idf_svc::log::EspLogger::initialize_default();
@@ -37,9 +35,7 @@ pub fn run(){
     let scl = peripherals.pins.gpio7;
     let config = I2cConfig::new().baudrate(100.kHz().into());
     let i2c_driver = I2cDriver::new(i2c, sda, scl, &config).expect("I2c Driver Error");
-    // let i2c_ref_cell = RefCell::new(i2c_driver);
     let i2c_bus = shared_bus::BusManagerSimple::new(i2c_driver);
-
 
     let pin_driver = PinDriver::input_output_od(peripherals.pins.gpio10).unwrap();
     let mut one_wire_bus = OneWire::new(pin_driver).unwrap();
@@ -50,14 +46,14 @@ pub fn run(){
     let mut uart = AsyncUartDriver::new(peripherals.uart1, tx, rx, Option::<gpio::Gpio0>::None, Option::<gpio::Gpio1>::None, &uart_config).unwrap();
 
     let spi = peripherals.spi2;
-    let sclk = peripherals.pins.gpio18;
-    let serial_in = peripherals.pins.gpio19; // SDI
-    let serial_out = peripherals.pins.gpio20; // SDO
-    let cs = peripherals.pins.gpio21;
+    let sclk = peripherals.pins.gpio19;
+    let serial_in = peripherals.pins.gpio20; // SDI
+    let serial_out = peripherals.pins.gpio21; // SDO
+    let cs = peripherals.pins.gpio22;
 
     let driver = SpiDriver::new::<SPI2>(spi,sclk,serial_out,Some(serial_in),&SpiDriverConfig::new()).unwrap();
     let mut delay = Delay::new(10);
-    let spi_config = SPIConfig::new().baudrate(26.MHz().into());
+    let spi_config = SPIConfig::new().baudrate(26.MHz().into()).data_mode(MODE_3);
     let mut spi_device = SpiDeviceDriver::new(&driver, Some(cs), &spi_config).unwrap();
 
     let mut buf = [0_u8; 82];
@@ -109,7 +105,6 @@ pub fn run(){
                 println!("ang: {:?}, temp: {:?}, gyro: {:?}, acc: {:?}", ang, temp, gyro, acc);
             }
 
-
             // Read values from GPS
             {
                 while buf[0] != 36 {
@@ -118,14 +113,11 @@ pub fn run(){
                 rec.extend_from_slice(&buf);
                 // println!("Received NMEA: {:?}", rec);
                 
-                // Crop array to next iteratoin of 35
-
                 if let Some(index) = rec.iter().position(|&x| x == 13) {
                     rec.resize(index, 0);
                 }
 
                 // println!("Print received sentence {:?}", String::from_utf8(rec.clone()));
-
                 if rec.len() !=0 {
                     // println!("Received NMEA {:?}", rec);
                     let mut nmea_sentence = match String::from_utf8(rec.clone()) {
@@ -166,60 +158,21 @@ pub fn run(){
                 }
             }
 
+            {
+                // Write into SD Card
+                // Not working
+                // let sdcard = SdCard::new(&mut spi_device, delay);
+                // println!("Card size is {} bytes", sdcard.num_bytes().unwrap());
+                // println!("Write to SD Card ...");
 
-            // // Write into SD Card
-            // let sdcard = SdCard::new(&mut spi_device, delay);
-            // println!("Card size is {} bytes", sdcard.num_bytes().unwrap());
-            // println!("Write to SD Card ...");
+                // let mut response = [0u8; 1];
+                // spi_device.transfer(&mut response, &[0xFF])?;
+                // println!("SPI Response: {:?}", response);
 
-            // // 1 second delay
-            // FreeRtos::delay_ms(1000);
+                // // 1 second delay
+                // FreeRtos::delay_ms(1000);
+            }
 
         }
     })
-
 }
-
-    // // initialize peripherals
-    // // let peripherals = Peripherals::take().expect("Unable to access device peripheral");
-    // // let spi = peripherals.spi2;
-
-    // // initialize pins
-    // // let i2c = peripherals.i2c0;
-    // // let i2c_ref_cell = RefCell::new(i2c);
-    // // let mut sda = SharedPin::new(peripherals.pins.gpio6);
-    // // let mut scl = SharedPin::new(peripherals.pins.gpio7);
-    // // let mut rx = SharedPin::new(peripherals.pins.gpio17);
-    // // let mut tx = SharedPin::new(peripherals.pins.gpio16);
-
-    // // let sda = peripherals.pins.gpio6;
-    // // let scl = peripherals.pins.gpio7;
-
-
-
-    // // initialize drivers
-    
-    // let config = I2cConfig::new().baudrate(100.kHz().into());
-
-    // let i2c_driver = i2c_bus::RefCellDevice::new(&i2c_ref_cell);
-
-    // // let i2c_driver_ina= I2cDriver::new(i2c, &sda, &scl, &config).expect("I2c Driver Error");
-    // // let i2c_driver_mpu= I2cDriver::new(i2c, sda_mpu, scl_mpu, &config).expect("I2c Driver Error");
-
-    // // initialize buses
-
-    // // initialize drivers
-
-    // // initialize devices
-    // let mut ina = SyncIna219::new(i2c_driver, Address::from_byte(0x40).expect("Out of bounds")).expect("INA219 Not detected");  
-    
-    // let mut mpu = Mpu6050::new(i2c_driver);
-    // mpu.init(&mut delay).expect("MPU Initialization Error");
-
-    // SD Card
-    // let sd_card = SdMmcSpi::new(spi_device);
-    // let mut volume_mgr = VolumeManager::<_, FatVolume>::new(sd_card, 1);
-    // let volume = volume_mgr.get_volume(0).unwrap();
-    // let root_dir = volume.open_root_dir().unwrap();
-    // let mut file = root_dir.open_file_in_dir("test.txt", embedded_sdmmc::Mode::ReadWriteCreate).unwrap();
-    // file.write(b"Hello, ESP32-C6!").unwrap();
